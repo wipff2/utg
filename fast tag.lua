abfca685b12aa5d1d976f2a12c42fd82f48d349f
wipff2 <kl.comp.me@gmail.com>
2025-03-27 16:14:08 +0800

    y

--- fast tag.lua
+++ fast tag.lua
@@ -1,26 +1,21 @@
--- Script Anti Cooldown
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-
--- Menonaktifkan cooldown pada `shared.cooldowns`
 if ReplicatedStorage:FindFirstChild("shared") then
     local shared = ReplicatedStorage.shared
     if shared:FindFirstChild("cooldowns") then
-        shared.cooldowns:Destroy() -- Menghapus objek cooldown jika ditemukan
-        print("Cooldown berhasil dihapus!")
+        shared.cooldowns:Destroy()
+        print("SUCCES")
     end
 end
-
--- Menonaktifkan fungsi `InCooldown`
 local success, err = pcall(function()
     local Utils = require(game.ReplicatedFirst:FindFirstChild("Utils"))
     if Utils and typeof(Utils.InCooldown) == "function" then
         hookfunction(Utils.InCooldown, function(action)
-            return false -- Cooldown selalu mati
+            return false
         end)
-        print("Hook fungsi InCooldown berhasil!")
+        print("SUCCES")
     end
 end)
-
 if not success then
-    warn("Gagal menghapus cooldown:", err)
+    warn("ERROR")
+    return
 end
